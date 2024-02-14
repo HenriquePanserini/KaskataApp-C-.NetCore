26a93faa0c1936a72efe5242acbf0ca2e5c502d6
Henrique Panserini <henrique.panserini@gmail.com>
2024-02-14 17:30:34 -0300

    Criação de Generics e IServices

---
+++ KaskataDDD.Domain/Interface/Generics/IGenerics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaskataDDD.Domain.Interface.Generics
+{
+    public interface IGenerics<T> where T : class
+    {
+        Task Create(T objeto);
+        Task Upgrade(T objeto);
+        Task Delete(T objeto);
+        Task<T> GetId(int id);
+        Task<List<T>> List();
+
+    }
+}
