e027d7047c3305bf214287d39302b1b260e1bcd3
Henrique Panserini <henrique.panserini@gmail.com>
2024-02-17 15:37:26 -0300

    Criação de repositorio e estrutura de ação Genericas

--- KaskataDDD.Infrastructure/Configuration/Context.cs
+++ KaskataDDD.Infrastructure/Configuration/Context.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using KaskataDDD.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KaskataDDD.Infrastructure.Configuration
 {
-    internal class Context
+    internal class Context : IdentityDbContext<ApplicationUser>
     {
+        public Context(DbContextOptions<Context> options) :  base(options)
+        {  
+        
+        }
+
+        public DbSet<News> News { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(e => e.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ConnectionString()
+        {
+            string strcon = "Data Source=DESKTOP-JDDJMQ9\\SQLEXPRESS; Initial Catalog=API_KASKATA_2024; Integrated Security=False; User ID=DESKTOP-JDDJMQ9\\henri;";
+            return strcon;
+        }
     }
 }
---
+++ KaskataDDD.Infrastructure/Repository/Generic/RepositoryGeneric.cs
@@ -0,0 +1,70 @@
+﻿using KaskataDDD.Domain.Interface.Generics;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaskataDDD.Infrastructure.Repository.Generic
+{
+    public class RepositoryGeneric<T> : IGenerics<T>, IDisposable where T : class
+    {
+        public async Task Create(T objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Delete(T objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<T>> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Upgrade(T objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Disposed https://docs.microsoft.com/pt-br/dotnet/standard/garbage-collection/implementing-dispose
+        // Flag: Has Dispose already been called?
+        bool disposed = false;
+        // Instantiate a SafeHandle instance.
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+                // Free any other managed objects here.
+                //
+            }
+
+            disposed = true;
+        }
+        #endregion
+    }
+}
